100e7182267862335ee576b22795fedfd15099a2
SHUAIJUN CHEN <chenshua@deakin.edu.au>
2020-05-22 12:02:58 +0800

    project framework and basic UI design

--- Assessment-2/app/src/main/java/com/example/assessment_2/MainActivity.java
+++ Assessment-2/app/src/main/java/com/example/assessment_2/MainActivity.java
@@ -1,113 +1,180 @@
 package com.example.assessment_2;
 
-//import android.app.Activity;
-//import android.graphics.BitmapFactory;
-//import android.graphics.Matrix;
-//import android.os.Bundle;
-//import android.util.DisplayMetrics;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.animation.Animation;
-//import android.view.animation.TranslateAnimation;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//import androidx.viewpager.widget.PagerAdapter;
-//import androidx.viewpager.widget.ViewPager;
-
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    private ViewPager viewPage;
-    private ArrayList arrayList;
-    private View view_video, view_brand, view_map, view_login;
+public class MainActivity extends Activity implements View.OnClickListener, ViewPager.OnPageChangeListener {
+
+    private TextView txt_title;
+
+    // 底部菜单4个Linearlayout
+    private LinearLayout ll_home;
+    private LinearLayout ll_address;
+    private LinearLayout ll_friend;
+    private LinearLayout ll_setting;
+
+    // 底部菜单4个ImageView
+    private ImageView img_home;
+    private ImageView img_news;
+    private ImageView img_friend;
+    private ImageView img_user;
+
+    // 底部菜单4个菜单标题
+    private TextView txt_home;
+    private TextView txt_news;
+    private TextView txt_friend;
+    private TextView txt_user;
+
+    // 中间内容区域
+    private ViewPager viewPager;
+
+    // ViewPager适配器ContentAdapter
+    private ContentAdapter adapter;
+    private List<View> views;
 
     @Override
-    protected void onCreate(Bundle savedInstanceStat) {
-        super.onCreate(savedInstanceStat);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewPage = findViewById(R.id.viewPager);
+        initView();// 初始化控件
+        initEvent();// 设置按钮监听
 
-        LayoutInflater layoutInflater = getLayoutInflater();
-        view_video = layoutInflater.inflate(R.layout.page_video, null);
-        view_brand = layoutInflater.inflate(R.layout.page_brand, null);
-        view_map = layoutInflater.inflate(R.layout.page_map, null);
-        view_login = layoutInflater.inflate(R.layout.page_login, null);
+        //txt_title.setText(R.string.txt_nav_home);
+        txt_home.setTextColor(0xff1B940A);
+    }
 
-        List<View> viewList = new ArrayList<View>();
-        viewList.add(view_video);
-        viewList.add(view_brand);
-        viewList.add(view_map);
-        viewList.add(view_login);
-        MyPagerAdapter myPagerAdapter = new MyPagerAdapter(viewList);
-
-        //set viewpager adapter
-        viewPage.setAdapter(myPagerAdapter);
-
-        RadioGroup mainBtnContainer = (RadioGroup) findViewById(R.id.rg_main_button);
-
-        RadioButton btn_video = (RadioButton) findViewById(R.id.rb_video);
-        RadioButton btn_brand = (RadioButton) findViewById(R.id.rb_brand);
-        RadioButton btn_map = (RadioButton) findViewById(R.id.rb_map);
-        RadioButton btn_login = (RadioButton) findViewById(R.id.rb_login);
+    private void initEvent() {
+        ll_home.setOnClickListener(this);
+        ll_address.setOnClickListener(this);
+        ll_friend.setOnClickListener(this);
+        ll_setting.setOnClickListener(this);
+        viewPager.setOnPageChangeListener(this);
     }
 
     private void initView() {
-        //通过findViewById()来找到我们需要的RadioGroup
-        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.rg_main_button);
-        //设置状态改变的事件
-        radioGroup.setOnCheckedChangeListener((RadioGroup.OnCheckedChangeListener) this);
+        txt_title = findViewById(R.id.title);
 
-        viewPage.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        // 底部菜单4个Linearlayout
+        ll_home = findViewById(R.id.ll_home);
+        ll_address = findViewById(R.id.ll_brand);
+        ll_friend = findViewById(R.id.ll_map);
+        ll_setting = findViewById(R.id.ll_user);
 
-            }
-            /**
-             * 重写的状态改变的事件的方法
-             *
-             * @param //group     单选组合框
-             * @param //checkedId 其中的每个RadioButton的Id
-             */
+        // 底部菜单4个ImageView
+        img_home = findViewById(R.id.img_home);
+        img_news = findViewById(R.id.img_brand);
+        img_friend = findViewById(R.id.img_map);
+        img_user = findViewById(R.id.img_user);
 
-            //界面改变监听
-            //界面改变监听
-            //当前选中页面
-            @Override
-            public void onPageSelected(int position) {
-                switch (position){
-                    case 0:
-                        viewPage.check(R.id.rb_video);
-                        break;
-                    case 1:
-                        viewPage.check(R.id.rb_brand);
-                        break;
-                    case 2:
-                        viewPage.check(R.id.rb_map);
-                        break;
-                    case 3:
-                        viewPage.check(R.id.rb_login);
-                        break;
-                    default:
-                        break;
-                }
-            }
+        // 底部菜单4个菜单标题
+        txt_home = findViewById(R.id.txt_home);
+        txt_news = findViewById(R.id.txt_brand);
+        txt_friend = findViewById(R.id.txt_map);
+        txt_user = findViewById(R.id.txt_user);
 
-            @Override
-            public void onPageScrollStateChanged(int state) {
+        // 中间内容区域ViewPager
+        viewPager = findViewById(R.id.vp_content);
 
-            }
-        });
+        // 适配器
+        View page_home = View.inflate(MainActivity.this, R.layout.page_home, null);
+        View page_news = View.inflate(MainActivity.this, R.layout.page_brand, null);
+        View page_friend = View.inflate(MainActivity.this, R.layout.page_map, null);
+        View page_user = View.inflate(MainActivity.this, R.layout.page_user, null);
+
+        views = new ArrayList<View>();
+        views.add(page_home);
+        views.add(page_news);
+        views.add(page_friend);
+        views.add(page_user);
+        adapter = new ContentAdapter(views);
+        viewPager.setAdapter(adapter);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_home:
+                restartBotton();
+                //txt_title.setText(R.string.txt_nav_home);
+                txt_home.setTextColor(0xff1B940A);
+                viewPager.setCurrentItem(0);
+                break;
+            case R.id.ll_brand:
+                restartBotton();
+                //txt_title.setText(R.string.txt_nav_news);
+                txt_news.setTextColor(0xff1B940A);
+                viewPager.setCurrentItem(1);
+                break;
+            case R.id.ll_map:
+                restartBotton();
+                //txt_title.setText(R.string.txt_nav_friend);
+                txt_friend.setTextColor(0xff1B940A);
+                viewPager.setCurrentItem(2);
+                break;
+            case R.id.ll_user:
+                restartBotton();
+                //txt_title.setText(R.string.txt_nav_user);
+                txt_user.setTextColor(0xff1B940A);
+                viewPager.setCurrentItem(3);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        restartBotton();
+        switch (position) {
+            case 0:
+                //txt_title.setText(R.string.txt_nav_home);
+                txt_home.setTextColor(0xff1B940A);
+                break;
+            case 1:
+                //txt_title.setText(R.string.txt_nav_news);
+                txt_news.setTextColor(0xff1B940A);
+                break;
+            case 2:
+                //txt_title.setText(R.string.txt_nav_friend);
+                txt_friend.setTextColor(0xff1B940A);
+                break;
+            case 3:
+                //txt_title.setText(R.string.txt_nav_user);
+                txt_user.setTextColor(0xff1B940A);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    //重置链接样式
+    private void restartBotton() {
+        txt_home.setTextColor(Color.parseColor("#888888"));
+        txt_news.setTextColor(Color.parseColor("#888888"));
+        txt_friend.setTextColor(Color.parseColor("#888888"));
+        txt_user.setTextColor(Color.parseColor("#888888"));
     }
 }
--- Assessment-2/app/src/main/java/com/example/assessment_2/fragment_map.java
+++
@@ -1,6 +0,0 @@
-package com.example.assessment_2;
-
-import androidx.fragment.app.Fragment;
-
-public class fragment_map extends Fragment {
-}
